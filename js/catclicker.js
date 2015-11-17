9f1497299bf85b32a60bf0f2b7b64000818e5916
Udayan Shevade <udayan.shevade@gmail.com>
2015-11-16 19:11:36 -0800

    Add MOV model

--- js/catclicker.js
+++ js/catclicker.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var doc = document;
 
+
     var model = {
         init: function(n) {
             this.cats = [];
@@ -25,10 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 
+
     var octopus = {
         init: function() {
             model.init(5);
             view.init();
+        },
+        getAllCats: function() {
+            return model.cats;
         },
         getVisibleCat: function() {
             var visibleCat = model.cats.filter(function(cat) {
@@ -36,11 +41,21 @@ document.addEventListener('DOMContentLoaded', function() {
             });
             return visibleCat[0];
         },
+        changeVisibleCat: function(i) {
+            model.cats.forEach(function(cat) {
+                if (cat.visible) {
+                    cat.visible = false;
+                }
+            });
+            model.cats[i].visible = true;
+            view.render();
+        },
         upCount: function(cat) {
             model.upCount(cat.id);
-            view.render(cat);
+            view.render();
         }
-    }
+    };
+
 
     var view = {
         init: function() {
@@ -55,32 +70,53 @@ document.addEventListener('DOMContentLoaded', function() {
             this.counter.id = 'catCounter';
             this.name.id = 'catName';
             this.img.id = 'catImage';
-            this.selectList.id = 'select';
-
+            this.selectList.id = 'ul';
 
 
             // append elements
+            this.catContainer.appendChild(this.selectList);
             document.body.appendChild(this.catContainer);
             this.catContainer.appendChild(this.counter);
             this.catContainer.appendChild(this.name);
             this.catContainer.appendChild(this.img);
-            this.catContainer.appendChild(this.selectList);
 
             // render visible cat
             this.render();
 
+            // bind image clicks to appropriate image
             this.img.addEventListener('click', function() {
                 octopus.upCount(view.visibleCat);
             });
-        },
-        render: function(cat) {
-            this.visibleCat = octopus.getVisibleCat();
 
+            // return all current cats from model
+            var allCats = octopus.getAllCats();
+
+            // create list items for each cat
+            var i, listItem, thisCat;
+            for (i = 0, len = allCats.length; i < len; i++) {
+                thisCat = allCats[i];
+                listItem = doc.createElement('li');
+                listItem.className = 'select-li';
+                listItem.value = i;
+                listItem.innerText = thisCat.name;
+                this.selectList.appendChild(listItem);
+
+                listItem.addEventListener('click', (function (i, _this) {
+                    return function() {
+                        octopus.changeVisibleCat(i);
+                        _this.render();
+                    }
+                })(i, this));
+            }
+
+        },
+        render: function() {
+            this.visibleCat = octopus.getVisibleCat();
             this.img.src = this.visibleCat.src;
             this.name.innerText = this.visibleCat.name;
             this.counter.innerText = this.visibleCat.count;
         }
-    }
+    };
 
     octopus.init();
 
