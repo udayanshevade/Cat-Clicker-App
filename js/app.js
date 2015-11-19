4ca456d44caab42292d8093cc4960d3abe22d111
Udayan Shevade <udayan.shevade@gmail.com>
2015-11-19 00:18:03 -0800

    Add multiple cats and click selection

--- js/app.js
+++ js/app.js
@@ -1,10 +1,53 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/Tabby.jpg');
-  this.clickRanks = ko.observableArray(['Newborn', 'Infant', 'Kitten', 'Little Cat', 'Grande Cat', 'Venti Cat', 'Top Cat']);
-  this.nickNames = ko.observableArray(['TabTab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']);
-  this.clicksToNextRank = ko.observable(5);
+var initCats = [
+  {
+    name: 'Tabby',
+    imgSrc: 'img/Tabby.jpg',
+    clickCount: 0,
+    ranks: ['Newborn', 'Infact', 'Kitty', 'Little Cat', 'Grande Cat', 'Venti Cat', 'Top Cat'],
+    nickNames: ['Tabs', 'Tab-Tab', 'T-Bone', 'Mr. T'],
+    clicksToNextRank: 3
+  },
+  {
+    name: 'Tiger',
+    imgSrc: 'img/Tiger.jpg',
+    clickCount: 0,
+    ranks: ['Newborn', 'Infact', 'Kitty', 'Little Cat', 'Grande Cat', 'Venti Cat', 'Top Cat'],
+    nickNames: ['Tigger', 'Go-get-em', 'Sher Khan', 'Stripes'],
+    clicksToNextRank: 10
+  },
+  {
+    name: 'Scaredy',
+    imgSrc: 'img/Scaredy.jpg',
+    clickCount: 0,
+    ranks: ['Newborn', 'Infact', 'Kitty', 'Little Cat', 'Grande Cat', 'Venti Cat', 'Top Cat'],
+    nickNames: ['Fraidy', 'Jumpy', 'Casper'],
+    clicksToNextRank: 5
+  },
+  {
+    name: 'Shadow',
+    imgSrc: 'img/Shadow.jpg',
+    clickCount: 0,
+    ranks: ['Newborn', 'Infact', 'Kitty', 'Little Cat', 'Grande Cat', 'Venti Cat', 'Top Cat'],
+    nickNames: ['Shade', 'Ser Pounce', 'Shoobie'],
+    clicksToNextRank: 2
+  },
+  {
+    name: 'Sleepy',
+    imgSrc: 'img/Sleepy.jpg',
+    clickCount: 0,
+    ranks: ['Newborn', 'Infact', 'Kitty', 'Little Cat', 'Grande Cat', 'Venti Cat', 'Top Cat'],
+    nickNames: ['Snoozy', 'Nyquitty'],
+    clicksToNextRank: 6
+  },
+];
+
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.clickRanks = ko.observableArray(data.ranks);
+  this.nickNames = ko.observableArray(data.nickNames);
+  this.clicksToNextRank = ko.observable(data.clicksToNextRank);
 
   this.clickRanksLength = ko.computed(function() {
     return this.clickRanks().length;
@@ -22,15 +65,27 @@ var Cat = function() {
     }
     return this.clickRanks()[this.clickLevel()];
   }, this);
-
-}
+};
 
 
 var ViewModel = function() {
-  this.currentCat = ko.observable( new Cat() );
+
+  var self = this;
+
+  this.catList = ko.observableArray();
+
+  initCats.forEach(function(thisCat) {
+    self.catList.push( new Cat(thisCat) );
+  });
+
+  this.currentCat = ko.observable( this.catList()[0] );
   // increment the counter as per the clicks
   this.incrementCounter = function() {
-    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    this.clickCount(this.clickCount() + 1);
+  };
+
+  this.changeCat = function(clickedCat) {
+    self.currentCat(clickedCat);
   };
 
 
